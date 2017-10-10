963554deb4ae02eeab31a64aba93850379583330
HoltkampN <32459759+HoltkampN@users.noreply.github.com>
2017-10-09 17:04:48 -0700

    Add files via upload

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myString = "Hello World";
+
+            var myOtherString = myString;
+
+            Console.WriteLine(myString);
+            Console.WriteLine(myOtherString);
+
+            var myInputString = Console.ReadLine();
+
+            Console.WriteLine("my input: " + myInputString);
+            Console.ReadLine();
+
+            var myDecimal = 100.1m;
+
+            myDecimal += 1.34m;
+
+           
+
+
+                
+            
+
+        }
+    }
+}
